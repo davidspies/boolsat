5e423be33b0224191e98713305cc331da618bce5
David Spies <dnspies@gmail.com>
2018-08-01 22:21:59 -0700

    Map assignments to clauses

--- src/BoolSat/Solver/DPLL.hs
+++ src/BoolSat/Solver/DPLL.hs
@@ -1,14 +1,14 @@
-{-# OPTIONS_GHC -Wno-unused-matches #-}
-
 module BoolSat.Solver.DPLL
   ( DPLL(..)
   )
 where
 
 import           Control.Monad
+import qualified Data.DList                    as DList
 import           Data.Maybe
-import qualified Data.Set                      as Set
 import           Data.Set                       ( Set )
+import qualified Data.Set                      as Set
+import           Data.Map                       ( Map )
 import qualified Data.Map                      as Map
 import           Data.Foldable                  ( find )
 
@@ -20,13 +20,16 @@ data DPLL = DPLL
 instance Solver DPLL where
   solve DPLL = solution
 
+data Context = Context (Map Assignment [Disjunction]) Problem
+
 solution :: Problem -> [Solution]
 solution prob = go =<< inference emptySol
  where
   vars :: Set Variable
   vars = allVars prob
+  ctx  = contextOf prob
   inference :: Solution -> [Solution]
-  inference = maybeToList . allInferenceSteps prob
+  inference = maybeToList . allInferenceSteps ctx
   go :: Solution -> [Solution]
   go sol = case makeNextChoice vars sol of
     Nothing -> do
@@ -37,8 +40,7 @@ solution prob = go =<< inference emptySol
       go =<< inference (addAssignment a sol)
 
 addAssignment :: Assignment -> Solution -> Solution
-addAssignment (Assignment v b) (Solution sol) =
-  Solution $ Map.insert v b sol
+addAssignment (Assignment v b) (Solution sol) = Solution $ Map.insert v b sol
 
 makeNextChoice :: Set Variable -> Solution -> Maybe Variable
 makeNextChoice vs (Solution sol) = find (`Map.notMember` sol) vs
@@ -46,7 +48,18 @@ makeNextChoice vs (Solution sol) = find (`Map.notMember` sol) vs
 emptySol :: Solution
 emptySol = makeSolution []
 
-type Inference = Problem -> Solution -> Maybe Solution
+contextOf :: Problem -> Context
+contextOf prob@(Problem disjs) = Context
+  (Map.map DList.toList $ Map.fromListWith
+    (<>)
+    [ (a, DList.singleton d)
+    | d@(Disjunction assigns) <- disjs
+    , a                       <- Set.toList assigns
+    ]
+  )
+  prob
+
+type Inference = Context -> Solution -> Maybe Solution
 
 data UnitResult = NoLearn | Unsat | Assign Assignment
 
@@ -58,34 +71,40 @@ joinResults = \case
   (Assign a : xs) -> (a :) <$> joinResults xs
 
 unitPropogation, pureLiteralElimination, allInferenceSteps :: Inference
-unitPropogation (Problem disjs) = go
+unitPropogation (Context vm (Problem disjs)) = go disjs
  where
-  go sol@(Solution solAssigns) = case newAssigns of
+  go ds sol@(Solution solAssigns) = case newAssigns of
     Nothing         -> Nothing
     Just []         -> Just sol
-    Just na@(_ : _) -> go (foldl (flip addAssignment) sol na)
+    Just na@(_ : _) -> go
+      (concatMap
+        (\(Assignment var val) ->
+          fromMaybe [] $ vm Map.!? Assignment var (not val)
+        )
+        na
+      )
+      (foldl (flip addAssignment) sol na)
    where
     inverted :: Assignment -> Bool
-    inverted (Assignment v b) =
-      Map.lookup v solAssigns == Just (not b)
+    inverted (Assignment v b) = Map.lookup v solAssigns == Just (not b)
     newAssigns :: Maybe [Assignment]
-    newAssigns = joinResults $ (`map` disjs) $ \(Disjunction d) ->
+    newAssigns = joinResults $ (`map` ds) $ \(Disjunction d) ->
       case filter (not . inverted) (Set.toList d) of
         []      -> Unsat
         [a@(Assignment v _)] | v `Map.notMember` solAssigns -> Assign a
         (_ : _) -> NoLearn
 
-pureLiteralElimination prob@(Problem disjs) sol =
-  Just $ foldl (flip addAssignment) sol $ mapMaybe pureValue
-                                                   (Set.toList $ allVars prob)
+pureLiteralElimination (Context vm prob) sol@(Solution solAssigns) =
+  Just $ foldl (flip addAssignment) sol $ mapMaybe
+    pureValue
+    (filter (`Map.notMember` solAssigns) $ Set.toList $ allVars prob)
  where
   occursWithSign :: Variable -> Bool -> Bool
-  occursWithSign var val =
-    any (\(Disjunction bs) -> Assignment var val `Set.member` bs) disjs
+  occursWithSign var val = any (not . satisfiesConstraint sol)
+                               (fromMaybe [] $ vm Map.!? Assignment var val)
   pureValue :: Variable -> Maybe Assignment
-  pureValue var
-    | not (occursWithSign var True)  = Just (Assignment var False)
-    | not (occursWithSign var False) = Just (Assignment var True)
-    | otherwise                      = Nothing
+  pureValue var | not (occursWithSign var True)  = Just (Assignment var False)
+                | not (occursWithSign var False) = Just (Assignment var True)
+                | otherwise                      = Nothing
 
 allInferenceSteps prob = pureLiteralElimination prob <=< unitPropogation prob
